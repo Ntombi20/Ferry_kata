51cac008314126642e682ae174f45e528a4eac7b
ntombi20 <ntombi@projectcodex.co>
2016-09-20 14:22:34 +0200

    files

---
+++ car.js
@@ -0,0 +1,6 @@
+exports.cars = function (colour, number) {
+  // body...
+  var colour = ("red", "blue", "black", "orange", "white", "purple");
+  var numberOfPeople = (1, 2, 3, 4, 5, 6);
+
+};
---
+++ test/ferry_test.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+var car = require('./car');
+
+describe("The number of cars and people that can get onto a Ferry.", function(done) {
+  it("Should specify the colour and the number of passengers in the car.", function(done){
+
+    assert.equal(1,2);
+    done;
+  })
+  done;
+});
